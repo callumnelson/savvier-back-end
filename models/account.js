fb1874aaf069f072fa679e7e833706b3f20ad86a
Callum Nelson <callum.nelson95@gmail.com>
2023-06-01 17:22:28 -0400

    initial commit

---
+++ models/account.js
@@ -0,0 +1,38 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Account extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Account.init({
+    name: {
+      type: DataTypes.STRING,
+    },
+    type: {
+      type: DataTypes.ENUM,
+      values: ['Credit Card', 'Checking', 'Savings', 'Other']
+    },
+    profileId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      onDelete: 'CASCADE',
+      references: {
+        model: 'Profiles', 
+        key: 'id',
+      }
+    },
+  }, {
+    sequelize,
+    modelName: 'Account',
+  });
+  return Account;
+};
